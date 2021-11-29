24c8cbdb9906d11ba62965fc16a49aa76b8ee3e1
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-29 04:53:39 -0500

    user welcome message

--- Tivi/Tivi/OldUserForm.cs
+++ Tivi/Tivi/OldUserForm.cs
@@ -12,9 +12,22 @@ namespace Tivi
 {
     public partial class OldUserForm : Form
     {
+        private User user;
+
         public OldUserForm()
         {
             InitializeComponent();
+        }
+
+        public OldUserForm(User user)
+        {
+            InitializeComponent();
+            this.user = new User(user); //deep copy
+        }
+
+        private void OldUserForm_Load(object sender, EventArgs e)
+        {
+            userLabel.Text = user.FirstName + "!";
         }
     }
 }
