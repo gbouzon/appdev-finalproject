b9c308e36d89fab6917364e310318b5ee4d5cc79
Chilka <chilcjcastro@yahoo.ca>
2021-12-05 00:22:52 -0500

    Added stopwatch

--- Tivi/Tivi/StopWatchForm.Designer.cs
+++ Tivi/Tivi/StopWatchForm.Designer.cs
@@ -28,19 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.minutesLabel = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.stopWatchTimer = new System.Windows.Forms.Timer(this.components);
+            this.colonLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.microSecondsLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.secondsLabel.Location = new System.Drawing.Point(56, 34);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(101, 78);
+            this.secondsLabel.TabIndex = 0;
+            this.secondsLabel.Text = "00";
+            // 
+            // minutesLabel
+            // 
+            this.minutesLabel.AutoSize = true;
+            this.minutesLabel.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.minutesLabel.Location = new System.Drawing.Point(207, 34);
+            this.minutesLabel.Name = "minutesLabel";
+            this.minutesLabel.Size = new System.Drawing.Size(101, 78);
+            this.minutesLabel.TabIndex = 1;
+            this.minutesLabel.Text = "00";
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.hoursLabel.Location = new System.Drawing.Point(343, 34);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(101, 78);
+            this.hoursLabel.TabIndex = 2;
+            this.hoursLabel.Text = "00";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(69, 127);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(121, 49);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(253, 127);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(121, 49);
+            this.pauseButton.TabIndex = 4;
+            this.pauseButton.Text = "PAUSE";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(437, 127);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(121, 49);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // stopWatchTimer
+            // 
+            this.stopWatchTimer.Tick += new System.EventHandler(this.stopWatchTimer_Tick);
+            // 
+            // colonLabel
+            // 
+            this.colonLabel.AutoSize = true;
+            this.colonLabel.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colonLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.colonLabel.Location = new System.Drawing.Point(301, 34);
+            this.colonLabel.Name = "colonLabel";
+            this.colonLabel.Size = new System.Drawing.Size(49, 78);
+            this.colonLabel.TabIndex = 6;
+            this.colonLabel.Text = ":";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(152, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 78);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ":";
+            // 
+            // microSecondsLabel
+            // 
+            this.microSecondsLabel.AutoSize = true;
+            this.microSecondsLabel.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.microSecondsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.microSecondsLabel.Location = new System.Drawing.Point(484, 34);
+            this.microSecondsLabel.Name = "microSecondsLabel";
+            this.microSecondsLabel.Size = new System.Drawing.Size(101, 78);
+            this.microSecondsLabel.TabIndex = 8;
+            this.microSecondsLabel.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(438, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 78);
+            this.label3.TabIndex = 9;
+            this.label3.Text = ":";
             // 
             // StopWatchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(609, 387);
+            this.ClientSize = new System.Drawing.Size(642, 214);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.microSecondsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colonLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.hoursLabel);
+            this.Controls.Add(this.minutesLabel);
+            this.Controls.Add(this.secondsLabel);
             this.Name = "StopWatchForm";
             this.Text = "StopWatchForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label minutesLabel;
+        private System.Windows.Forms.Label hoursLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer stopWatchTimer;
+        private System.Windows.Forms.Label colonLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label microSecondsLabel;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Tivi/Tivi/StopWatchForm.cs
+++ Tivi/Tivi/StopWatchForm.cs
@@ -12,9 +12,84 @@ namespace Tivi
 {
     public partial class StopWatchForm : Form
     {
+        private int seconds;
+        private int minutes;
+        private int hours;
+        private int microseconds;
+
         public StopWatchForm()
         {
             InitializeComponent();
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+            microseconds = 0;
+
+        }
+
+        private void stopWatchTimer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            microseconds++;
+
+            if (microseconds > 9)
+            {
+                seconds++;
+                microseconds = 0;
+            }
+
+            if (seconds > 59)
+            {
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes > 59)
+            {
+                hours++;
+                minutes = 0;
+            }
+
+            hoursLabel.Text = appendZero(hours);
+            minutesLabel.Text = appendZero(minutes);
+            secondsLabel.Text = appendZero(seconds);
+            microSecondsLabel.Text = appendZero(microseconds);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            stopWatchTimer.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopWatchTimer.Stop();
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+
+            hoursLabel.Text = appendZero(hours);
+            minutesLabel.Text = appendZero(minutes);
+            secondsLabel.Text = appendZero(seconds);
+            microSecondsLabel.Text = appendZero(microseconds);
+        }
+
+        private string appendZero(double str)
+        {
+            if (str <= 9)
+            {
+                return "0" + str;
+            }
+            else
+            {
+                return str.ToString();
+            }
+
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            stopWatchTimer.Stop();
         }
     }
 }
