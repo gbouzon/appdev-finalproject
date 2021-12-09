41cd3ba99ff21bebebc4216dbfb05a99ed96ddf2
gracexia02 <gracexia2002@gmail.com>
2021-12-08 19:55:57 -0500

    add icon for budget and comment the message box in to do form

--- Tivi/Tivi/Properties/Resources.Designer.cs
+++ Tivi/Tivi/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Tivi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon budget {
+            get {
+                object obj = ResourceManager.GetObject("budget", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon calendar {
             get {
                 object obj = ResourceManager.GetObject("calendar", resourceCulture);
